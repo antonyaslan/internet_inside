9728d0c426ebe811d07bdea599fe3811fbf6fca3
antonyaslan <antonyaslan@gmail.com>
2024-04-25 11:23:35 +0200

    Application localy working and requirements updated for tun device

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-import struct
 import board
+import queue
 from digitalio import DigitalInOut
 from rf24 import RF24
 import spidev
@@ -8,48 +8,47 @@ import threading
 import time
 from tuntap import TunTap
 
-# Define tun device
+""" Define tun device """
 tun = TunTap(nic_type="Tun", nic_name="LongG")
 
 
 cond_in = threading.Condition()
-tun_in_queue = []
+tun_in_queue = queue.Queue()
 
 cond_out = threading.Condition()
-tun_out_queue = []
+tun_out_queue = queue.Queue()
 
-# define Radios
-#rf_rx = RF24(None, None, None, None, None)
-#nrf_tx = RF24(None, None, None, None, None)
-
-# invalid default values for scoping
+""" Invalid default values for scoping """
 SPI_BUS, CSN_PIN, CE_PIN = (None, None, None)
 
-# Constants for CE & CSN pins for the two SPI buses
+""" Constants for CE & CSN pins for the two SPI buses """
+
 CE_BUS_0 = board.D22
 CE_BUS_1 = board.D24
 CSN_BUS_0 = 0
 CSN_BUS_1 = 10
 
-# We only use device 0 for the two SPI buses
+""" We only use device 0 for the two SPI buses """
+
 SPI_DEV = 0
 
-#Radio Parameters
+""" Radio Parameters """
+
 POWER_LEVEL = -12 #DBM
 DELAY = 15 #e-15
 COUNT = 5
 DATA_RATE = 2 #MBps
 CRC_LENGTH = 8 #Bytes
 
-#Fragments size
+""" Fragments size """
 FRAG_SIZE = 30
 
 
-# Setup the two radios
+""" Setup the two radios """
 def setup(role) -> (RF24, RF24):
-    # configure tun device
+    
     if role == 1:
-        # Node
+        """ Mobile """
         tun.config(ip="192.168.1.2", mask="255.255.255.0")
 
         command = 'ip route add 8.8.8.8 via 192.168.1.1 dev LongG'
@@ -57,7 +56,7 @@ def setup(role) -> (RF24, RF24):
 
 
     if role == 0:
-        # Base
+        """ Base """
         tun.config(ip="192.168.1.1", mask="255.255.255.0")
         subprocess.run('iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE', shell=True)
         command = 'sudo iptables -A FORWARD -i eth0 -o LongG -m state --state RELATED,ESTABLISHED -j ACCEPT'
@@ -65,50 +64,50 @@ def setup(role) -> (RF24, RF24):
         subprocess.run('sudo iptables -A FORWARD -i LongG -o eth0 -j ACCEPT', shell=True)
 
 
-    # Create SPI bus object
-    SPI_BUS_RX = spidev.SpiDev()  # for a faster interface on linux
+    """ Create SPI bus object """
+    SPI_BUS_RX = spidev.SpiDev()
     SPI_BUS_TX = spidev.SpiDev()
 
-    # On Linux, csn value is a bit coded
-    #                 0 = bus 0, CE0 - /dev/spidev0.0, SpiDev 0  
-    #                10 = bus 1, CE0 - /dev/spidev1.0, SpiDev 1
-    # Radio 0
+    """ Radio 0 """
     CE_PIN_0 = DigitalInOut(board.D22)
     CSN_PIN_0 = CSN_BUS_0
     SPI_BUS_NUM_0 = 0
-    # Radio 1
+    """ Radio 1 """
     CE_PIN_1 = DigitalInOut(board.D24)
     CSN_PIN_1 = CSN_BUS_1 
     SPI_BUS_NUM_1 = 1
 
-    # initialize the nRF24L01 on the spi bus object with the specified CE & CSN pin,
-    # at the specified bus and device /dev/spidev{bus}.{device}
+    """ 
+        Initialize the nRF24L01 on the spi bus object with the specified CE & CSN pin:
+        At the specified bus and device /dev/spidev{bus}.{device}
+    """
     nrf_rx = RF24(SPI_BUS_RX, CSN_PIN_0, CE_PIN_0, SPI_BUS_NUM_0, SPI_DEV)
     nrf_tx = RF24(SPI_BUS_TX, CSN_PIN_1, CE_PIN_1, SPI_BUS_NUM_1, SPI_DEV)
 
 
 
-    # set the Power Amplifier level to -12 dBm since this test example is
-    # usually run with nRF24L01 transceivers in close proximity
+    """ 
+        Set the Power Amplifier level to -12 dBm.
+        
+        Usually run with nRF24L01 transceivers in close proximity 
+    """
     nrf_rx.pa_level = POWER_LEVEL
     nrf_tx.pa_level = POWER_LEVEL
 
-    # set the number of connection retries and the the delay between each try
+    """ Set the number of connection retries and the the delay between each try """
     nrf_rx.set_auto_retries(DELAY, COUNT)
     nrf_tx.set_auto_retries(DELAY, COUNT)
 
-    # set the data_rate
+    """ Set the data_rate """
     nrf_rx.data_rate = DATA_RATE
     nrf_tx.data_rate = DATA_RATE
 
-    # enable the Dynamic Payloads
+    """ Εnable the Dynamic Payloads """
     nrf_rx.crc = CRC_LENGTH
     nrf_tx.crc = CRC_LENGTH
 
-    #nrf_rx.print_details()
-    #nrf_tx.print_details()
 
-    # addresses needs to be in a buffer protocol object (bytearray)
+    """ Αddresses needs to be in a buffer protocol object (bytearray)"""
     address = [b"Base", b"Node"]
 
     nrf_tx.open_tx_pipe(address[role])
@@ -154,7 +153,7 @@ def tx(nrf_tx: RF24, packet: bytes):
         packet (bytes): bytes to be transmitted
 
     """
-    nrf_tx.stopListening()
+    nrf_tx.listen = False
     fragments = fragment(packet)
 
     for frag in fragments:
@@ -166,10 +165,10 @@ def tx(nrf_tx: RF24, packet: bytes):
 
 def radio_tx(nrf_tx: RF24):
     while True:
-        with cond_in:
-            while not len(tun_in_queue) > 0:
-                cond_in.wait()
-            packet = tun_in_queue.pop()
+        #with cond_in:
+            #while not len(tun_in_queue) > 0:
+                #cond_in.wait()
+        packet = tun_in_queue.get()
         tx(nrf_tx, packet)
 
 def tun_rx():
@@ -178,49 +177,51 @@ def tun_rx():
     """
     while True:
         buffer = tun.read()
-        if len(buffer):
-            print("Got package from tun interface:\n\t", buffer, "\n")
-            tx(buffer)
+        tun_in_queue.put(buffer)
+        #if len(buffer):
+        #    print("Got package from tun interface:\n\t", buffer, "\n")
+        #    tx(buffer)
 
 def radio_rx(nrf_rx:RF24):
     """ Waits for incoming packet on reading pipe 
     and forwards the packet to tun interface
     """
     nrf_rx.listen = True
+
     buffer = []
     while True:
         has_payload = nrf_rx.available()
         if has_payload:
-            pSize = nrf_rx.get_payload_length()
-            fragment = nrf_rx.read(pSize)
+            pSize = nrf_rx.get_payload_length(nrf_rx.pipe)
+            fragment = nrf_rx.read()
             id = int.from_bytes(fragment[:2], 'big')
-            #print("Frag received with id: ", id)
+            print("Frag received with id: ", id)
 
             buffer.append(fragment[2:])
 
             if id == 0xFFFF:  # packet is fragmented and this is the first fragment
                 packet = b''.join(buffer)
-                #print("Packet received:\n\t", packet, "\n")
+                print("Packet received:\n\t", packet, "\n")
                 buffer.clear()
-
-                with cond_out:
-                    tun_out_queue.append(packet)
-                    cond_out.notify()
-
+                tun_out_queue.put(packet)
+                #with cond_out:
+                #    tun_out_queue.append(packet)
+                #    cond_out.notify() 
+    
 def tun_tx():
 
     while True:
-        with cond_out:
-            while not len(tun_out_queue) > 0:
-                cond_out.wait()
-            packet = tun_out_queue.pop()
-
+        #with cond_out:
+        #    while not len(tun_out_queue) > 0:
+        #        cond_out.wait()
+        packet = tun_out_queue.get()
         tun.write(packet)
 
 def main():
-    rx_radio, tx_radio = setup(1)
-    radio_rx_thread = threading.Thread(target=radio_rx(rx_radio), args=())
-    radio_tx_thread = threading.Thread(target=radio_tx(tx_radio), args=())
+    node = int(input("Select node role. 0:Base 1:Mobile :"))
+    rx_radio, tx_radio = setup(node)
+    radio_rx_thread = threading.Thread(target=radio_rx, args=(rx_radio,))
+    radio_tx_thread = threading.Thread(target=radio_tx, args=(tx_radio,))
     tun_rx_thread = threading.Thread(target=tun_rx, args=())
     tun_tx_thread = threading.Thread(target=tun_tx, args=())
     radio_rx_thread.start()
@@ -233,8 +234,6 @@ def main():
     radio_tx_thread.join()
     tun_rx_thread.join()
     tun_tx_thread.join()
-    #rx_radio.print_details()
-    #tx_radio.print_details()
 
 if __name__ == "__main__":
     main()
