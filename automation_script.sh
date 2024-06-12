2838c631a6a0d0a3a742c11ccc812e058cf1c217
Love Barany <lo5050ba-s@student.lu.se>
2024-06-12 10:53:41 +0200

    Cleanup repo a bit, update README, update automation script

--- automation_script.sh
+++ automation_script.sh
@@ -6,22 +6,18 @@ sudo apt install --upgrade python3-setuptools
 sudo apt install python3-venv
 python3 -m venv .env --system-site-packages
 #We need to work as root since we are working with TUN
-#source .env/bin/activate
-sudo pip3 install --upgrade adafruit-python-shell --break-system-packages
+sudo ./env/bin/pip3 install --upgrade adafruit-python-shell
 
 if ! [ -f ./raspi-blinka.py]; then
 	echo "Downloading Raspi Blinka drivers"
 	wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 fi
 
-sudo PATH=$PATH python3 raspi-blinka.py
+sudo .env/bin/python3 raspi-blinka.py
 dtoverlay=spi1-3cs
-sudo pip3 install -r requirements.txt --break-system-packages
+sudo .env/bin/pip3 install -r requirements.txt
+cd pyg_control_system
+maturin develop
+cd ..
+source .env/bin/activate
 clear
-sudo PATH=$PATH python3 pyg.py
-# Commented out since we use modified drivers
-# pip3 install circuitpython-nrf24l01
-# cd internet_inside
-# git clone https://github.com/2bndy5/CircuitPython_nRF24L01.git
-# cd CircuitPython_nRF24L01/examples
-# python3 nrf24l01_simple_test.py
